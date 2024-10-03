53967522a3c20cb714e2ac4a8dca48b0aebf48ce
Aditya Agarwal <136118380+Adityaagar2003@users.noreply.github.com>
2024-10-03 23:54:12 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,6 @@
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Initialize the map when DOM is fully loaded
-  var map = L.map('map').setView([-8.3405, 115.092], 13); // Bali, Indonesia
-
-  // Add OpenStreetMap tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-});
-
 
 
   const menuToggle = document.getElementById('menu-toggle');
@@ -22,35 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 
-
-  document.querySelectorAll('.faq-question').forEach(question => {
-    question.addEventListener('click', () => {
-      const faqItem = question.parentElement;
-
-      // Close all other open FAQ items
-      document.querySelectorAll('.faq-item').forEach(item => {
-        if (item !== faqItem) {
-          item.classList.remove('active');
-          item.querySelector('.faq-answer').style.maxHeight = null;
-          item.querySelector('.faq-toggle').textContent = '+';
-        }
-      });
-
-      // Toggle the clicked FAQ item
-      faqItem.classList.toggle('active');
-
-      const faqAnswer = faqItem.querySelector('.faq-answer');
-      const faqToggle = faqItem.querySelector('.faq-toggle');
-
-      if (faqItem.classList.contains('active')) {
-        faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
-        faqToggle.textContent = '-';
-      } else {
-        faqAnswer.style.maxHeight = null;
-        faqToggle.textContent = '+';
-      }
-    });
-  });
 
   
   // Get the modal
